7ff917bbe71effa73170fd83162871d7e7dd0062
Ernie1 <870949712@qq.com>
2018-05-20 00:01:22 +0800

    fff1

--- Jacobi_.m
+++ Jacobi_.m
@@ -15,65 +15,41 @@
 %     xlim([0,210]);
 %     legend('高斯消去法','列主元消去法');
 % end
-
-produceSolveComputeTime(500);
-
-function time1 = produceSolveComputeTime(size)
+[times1,time1,times2,time2]=produceSolveComputeTime(100)
+function [times1,time1,times2,time2] = produceSolveComputeTime(size)
+    %生成对称正定矩阵A，同时满足2D-A正定
     while 1
-        %A=sprandn(size,size,0.4);
         v=diag(rand(1,size));
         u=orth(randn(size));
         A=u*v*u^-1;
-%         vrho(A)%特征值
-        [D p1]=chol(A);
-        p1
-        %tem=2D-A
-        tem=2*diag(diag(A))-A;
-        [D p2]=chol(tem);
-        p2
+        %p1为0时A正定
+        [R,p1]=chol(A);
+        %2D-A
+        [R,p2]=chol(2*diag(diag(A))-A);
         if p1==0&&p2==0
             break;
         end
     end
-        x=rand(size,1);
-%         x=randn(size,1);
-        b=A*x;
-%  A=[4,    -1,    -1,     0;
-% 
-%     -1,     4,     0,    -1;
-% 
-%     -1,     0,     4,    -1;
-% 
-%      0,    -1,    -1,     4];
-%  b=[1;2;0;1];
-        [x,hasSol,time]=Jacobi(A,b)
-%         if hasSol==true
-%             break;
-%         end
-%      tic;
-%     Jacobi(A,b)
-%      time1=toc;
+    x=rand(size,1);
+    b=A*x;
+    tic;
+    [x,times1]=Jacobi(A,b);
+    time1=toc;
+    tic;
+    [x,times2]=GaussSeidel(A,b);
+    time2=toc;
 end
 
-function [x,hasSol,time]=Jacobi(A,b)
+function [x,times]=Jacobi(A,b)
     dim=size(A,1);
     x=zeros(dim,1);
     D=diag(diag(A));
-    %J=D^(-1)*(L+U);
+    %J=D^(-1)*(L+U)
     J=D\(-tril(A,-1)-triu(A,1));
-    hasSol=true;
-%     try
-%         if max(abs(eig(J)))>=1
-%             hasSol=false;
-%             return;
-%         end
-%     catch
-%         hasSol=false;
-%         return;
-%     end
+    %f=D^(-1)*b
     f=D\b;
     x_k=zeros(dim,1);
-    time=0;
+    times=0;
     while 1
         x=J*x_k+f;
         %精度，用无穷范数
@@ -82,8 +58,33 @@ function [x,hasSol,time]=Jacobi(A,b)
         end
         x_k=x;
         %迭代次数限制
-        time=time+1;
-        if time==2000000000
+        times=times+1;
+        if times==29000
+            error('超出迭代次数限制');
+        end
+    end
+end
+
+function [x,times]=GaussSeidel(A,b)
+    dim=size(A,1);
+    x=zeros(dim,1);
+    D=diag(diag(A));
+    %G=(D-L)^(-1)*U
+    G=(D+tril(A,-1))\(-triu(A,1));
+    %f=D^(-1)*b
+    f=(D+tril(A,-1))\b;
+    x_k=zeros(dim,1);
+    times=0;
+    while 1
+        x=G*x_k+f;
+        %精度，用无穷范数
+         if norm(x-x_k,inf)<1e-5
+            break;
+        end
+        x_k=x;
+        %迭代次数限制
+        times=times+1;
+        if times==29000
             error('超出迭代次数限制');
         end
     end
